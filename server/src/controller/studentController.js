19776aa5a2dcad4ec0f031a476a64097d76c625d
GabrielIkpolo <contact.gikps@gmail.com>
2024-05-13 11:39:36 +0100

    fixed some issues

--- server/src/controller/studentController.js
+++ server/src/controller/studentController.js
@@ -4,10 +4,39 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // Predefined array of departments
-const departments = ['Computer Science', 
-'Engineering', 'Mathematics', 'Physics', 'Biology', 'Civil Engineering', 'Political Science',
-'Law', 'Nursing'
+const departments = [
+    'Accounting',
+    'Agric Engineering',
+    'Anatomy',
+    'BCH',
+    'Biology',
+    'Busssiness Administration',
+    'Chemistry',
+    'Civil Engineering',
+    'Computer Engineering',
+    'Computer Information Science',
+    'Computer Information Technology',
+    'Computer Science',
+    'Cyber Security',
+    'Economics',
+    'Electrical Electronics Engr',
+    'Engineering',
+    'Law',
+    'LIS',
+    'mass Comm.',
+    'Mathematics',
+    'MCB',
+    'Mechanical Engineering',
+    'MLS',
+    'Nursing',
+    'Physics',
+    'Physiology',
+    'Political Science',
+    'Public Admin.',
+    'Public Health',
+    'Software Engineering'
 ];
+
 
 //Function to get all departments
 const getDepartments = (req, res) => {
